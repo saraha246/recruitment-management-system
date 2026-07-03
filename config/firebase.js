c860db3a6653f83da867152c37f9c7b98ca76a2c
Anugraha Santhanam <anu.san8016@gmail.com>
2026-07-03 16:20:22 +0530

    read Firebase service account from env variable for production

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,8 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getAuth } = require('firebase-admin/auth');
-const serviceAccount = require('../firebaseServiceAccount.json');
+
+// const serviceAccount = require('../firebaseServiceAccount.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 const app = initializeApp({
     credential: cert(serviceAccount)
